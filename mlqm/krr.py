692b09ba00aebb2c7446cf51b85540153132bc64
bgpeyton <bgpeyton@vt.edu>
2019-05-22 13:01:35 -0400

    Working on log-space grid search. Works for some clusterings but not others.

--- mlqm/krr.py
+++ mlqm/krr.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import random
 import copy
 
-def do_krr(M=12,N=50,st=0.05,s=5,l=0.0005,K=30):# {{{
+def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
     '''
     kernel ridge regression with M training points and N total representations
     st used for tatr generation
@@ -29,95 +29,105 @@ def do_krr(M=12,N=50,st=0.05,s=5,l=0.0005,K=30):# {{{
 #    tatr = make_tatr(mol,graph=True)# }}}
 
     # generate the total data set # {{{
-    tatr_list = [] # hold TATRs
-    E_CCSD_CORR_list = [] # hold CCSD correlation energy
-    E_SCF_list = [] # hold SCF energy
-    for i in range(0,N):
-        geom = """
-            H
-            H 1 """ + str(pes[i]) + """
-            symmetry c1
-        """
-        mol = mesp.Molecule('H2',geom,bas)
-        tatr = make_tatr(mol,150,st)
-        tatr_list.append(tatr)
-        E_CCSD_CORR_list.append(mol.E_CCSD_CORR)
-        E_SCF_list.append(mol.E_SCF)# }}}
+    try:
+        print("Loading data set . . .")
+        tatr_list = np.load('t_list.npy').tolist()
+        E_CCSD_CORR_list = np.load('corr_list.npy').tolist()
+        E_SCF_list = np.load('scf_list.npy').tolist()
+    except FileNotFoundError:
+        print("Generating data set . . .")
+        tatr_list = [] # hold TATRs
+        E_CCSD_CORR_list = [] # hold CCSD correlation energy
+        E_SCF_list = [] # hold SCF energy
+        for i in range(0,N):
+            geom = """
+                H
+                H 1 """ + str(pes[i]) + """
+                symmetry c1
+            """
+            mol = mesp.Molecule('H2',geom,bas)
+            tatr = make_tatr(mol,150,st)
+            tatr_list.append(tatr)
+            E_CCSD_CORR_list.append(mol.E_CCSD_CORR)
+            E_SCF_list.append(mol.E_SCF)
+        if save:
+            np.save('t_list.npy',tatr_list)
+            np.save('corr_list.npy',E_CCSD_CORR_list)
+            np.save('scf_list.npy',E_SCF_list)# }}}
 
-    E_CCSD_CORR_avg = np.mean(E_CCSD_CORR_list)
-    E_CCSD_CORR_list = np.subtract(E_CCSD_CORR_list,E_CCSD_CORR_avg)
-
-    # generate the training set and save their positions# {{{
+    # generate the training/validation sets# {{{
     print("Generating training set . . .")
     trainers = gen_train(tatr_list, M, K) 
+
+#    print("Using test training set.")
+#    trainers = sorted([152, 139, 35, 138, 112, 32, 106, 172, 57, 143, 52, 182], reverse=True) 
+    # s=10000,l=0 is good
+
+#    trainers = sorted([98, 116, 178, 47, 8, 138, 182, 32, 157, 75, 144, 143], reverse=True) 
+    # s=100,l=0 is good
+
+#    trainers = [182, 178, 174, 171, 144, 136, 125, 111, 109, 57, 47, 32]
+#    trainers = [184, 180, 176, 172, 144, 136, 124, 112, 108, 56, 48, 32]
+    # the above is completely composed of validation points- should get exact fit at these points
+
+    print("Training set: {}".format(trainers))
     t_pos = [pes[i] for i in trainers]
 
+    vals = sorted([i for i in range(0,200,4)], reverse=True) # validation set
+
     t_tatr = [] # training TATRs
-    t_E = [] # training energies
+    t_CORR_E = [] # training energies
+    v_tatr = [] # validation TATRs
+    v_CORR_E = [] # validation energies
+    v_SCF = [] # validation SCF energies
+    v_PES = [] # validation PES
+
     for i in trainers:
-        t_tatr.append(tatr_list[i])
-        t_E.append(E_CCSD_CORR_list[i])# }}}
+        t_tatr.append(tatr_list[i]) # assuming I don't have to pop the trainers out of set
+        t_CORR_E.append(E_CCSD_CORR_list[i])
+    for i in vals:
+#        vtatr = tatr_list.pop(i) # make _sure_ to pop vals out of set
+        vtatr = tatr_list[i]
+        vE = E_CCSD_CORR_list.pop(i)
+        v_tatr.append(vtatr)
+        v_CORR_E.append(vE)
+        v_SCF.append(E_SCF_list[i])
+        v_PES.append(pes[i])
 
-    # k-fold cross-validation to tune s
-    print("Cross-validating s . . .")
-    k = 5
-    step = 100
-    max_it = 1000
-    grad_conv = 1E-5 
-    s_new = s+1
-    s_list = [s,s_new]
+    # internally shift all corr E by the avg training set corr E
+    t_CORR_avg = np.mean(t_CORR_E)
+    t_CORR_E = np.subtract(t_CORR_E,t_CORR_avg)
+    v_CORR_E = np.subtract(v_CORR_E,t_CORR_avg)
+        # }}}
 
-    l_new = l+(l*0.5)
-    l_list = [l,l_new]
-
-    cv_error = []
-    cv_error_old, pred_E_list, mse_list = cross_validate(t_tatr,t_E,k,s,l)
-    cv_error = np.append(cv_error,cv_error_old)
-
-    for it in range(1,max_it+1):
-        cv_error_new, pred_E_list, mse_list = cross_validate(t_tatr,t_E,k,s_new,l)
-        cv_error = np.append(cv_error,cv_error_new)
-
-        grad = np.gradient(cv_error,s_list)
-        abs_grad = [abs(i) for i in grad]
-
-        if min(abs_grad) < grad_conv:
-            print("converged in {} iterations".format(it))
-            print("s value: {}".format(s_new))
-            print("l value: {}".format(l)) # s-only learning
-            #print("CV error: {}".format(cv_error_new))
-            break
-        elif it == max_it:
-            print("too many iterations")
-            print("s value: {}".format(s_new))
-            print("l value: {}".format(l)) # s-only learning
-            break
-        else:
-            s = s_new
-            s_new = s - step*grad[-1] # s-only learning
-            s_list.append(s_new)
-
-    print("final error (before prediction): {}".format(cv_error_new))
+    s_new, l = log_space_cv(t_CORR_E,t_tatr,s,l,k=M)
+#     USE BELOW FOR TESTING
+#    print("WARNING: Using static s and l values!")
+#    l = 0
+#    s_new = s 
+#    s_new = 5737.081137
+#    s_new = 11657780
+#    l = 7e-5
+#    l = 7e-26
 
     # train for alpha
-    alpha = train(t_tatr,t_E,s_new,l)
+    print("Model training using s = {} and l = {} . . .".format(s_new,l))
+    alpha = train(t_tatr,t_CORR_E,s_new,l)
 
     # predict E across the PES
-    pred_E_list = pred(tatr_list,t_tatr,alpha,s_new,l)
+    print("Predicting PES . . .")
+    pred_E_list = pred(v_tatr,t_tatr,alpha,s_new,l)
 
     # plot
-    E_CCSD_CORR_list = np.asarray(E_CCSD_CORR_list)
-    E_CCSD_CORR_list = np.add(E_CCSD_CORR_list,E_CCSD_CORR_avg)
-    E_SCF_list = np.asarray(E_SCF_list)
-    E_list = np.add(E_CCSD_CORR_list,E_SCF_list)
-    pred_E_list = np.asarray(pred_E_list)
-    pred_E_list = np.add(pred_E_list,E_CCSD_CORR_avg)
-    pred_E_list = np.add(pred_E_list,E_SCF_list)
+    v_E_list = np.add(v_CORR_E,t_CORR_avg)
+    v_E_list = np.add(v_E_list,v_SCF)
+    pred_E_list = np.add(pred_E_list,t_CORR_avg)
+    pred_E_list = np.add(pred_E_list,v_SCF)
 
     plt.figure(1)
-    plt.plot(pes,E_list,'b-o',label='CCSD PES')
-    plt.plot(pes,E_SCF_list,'y-',label='SCF')
-    plt.plot(pes,pred_E_list,'r^',ms=2,label='CCSD/ML-{}'.format(M))
+    plt.plot(v_PES,v_E_list,'b-o',label='CCSD PES',linewidth=3)
+    plt.plot(v_PES,v_SCF,'y-',label='SCF')
+    plt.plot(v_PES,pred_E_list,'r-^',ms=2,label='CCSD/ML-{}'.format(M),linewidth=2)
     plt.plot(t_pos,[-1.18 for i in range(0,len(t_pos))],'go',label='Training points')
     plt.axis([0.25,2.0,-2.5,-0.8])
     plt.xlabel('r/Angstrom')
@@ -277,26 +287,20 @@ def gen_train(tatr_list,M,K):# {{{
     Generate a training set given the list of TATRs and the number of training points
     Converge the k-means algorithm K separate times, choose best clustering
     Data points closest to cluster centers are chosen as trainers
+    Reverse the ordering so pop() can be done in succession on grand training set
     '''
     trainers = []
     t_map, close_pts = k_means_loop(tatr_list,M,K)
-    print("close pts: {}".format(close_pts))
     for train in range(0,M): # loop over centers, grab position of training points from each
-        print("center {}".format(train))
-        print("so it's pt {} in the t_map for center {}".format(close_pts[train],train))
-        check = t_map[train]
-        check2 = t_map[train][close_pts[train]]
         trainers.append(t_map[train][close_pts[train]][1])
-        print("trainers: {}".format(trainers))
-#    t_map, diffs = k_means_loop(tatr_list,M,K)
-#    for train in range(0,M): # loop over the centers, find the closest point to each
-#        trainers.append(np.argmin(diffs[train]))
-    return trainers# }}}
+    return sorted(trainers, reverse=True)# }}}
 
 def make_k(tatr1, tatr2, s):# {{{
     '''
     return a gaussian kernel from two TATRs
     '''
+    tatr1 = np.asarray(tatr1)
+    tatr2 = np.asarray(tatr2)
     tmp = la.norm(tatr1 - tatr2)
     return np.exp(-1 * tmp**2 / (2.0*s**2))# }}}
 
@@ -345,8 +349,17 @@ def pred(x,xt,a,s=0.05,l=0):# {{{
 
     # predict energy for the entire PES
     Y = [] # 
+#    lam = l*np.eye(N)[:M,:]
+#    lam = lam[:len(k[0])]
     for i in range(0,N):
+#        print("a is {} long\nk[{}] is {} long".format(len(a),i,len(k[i])))
         Y.append(np.dot(a,k[i]))
+#        print("k[{}] = {}".format(i,k[i]))
+#        print("lam = {}".format(lam))
+#        print("lam is {}x{}".format(len(lam),len(lam)))
+#        print("lam[{}] is size {}".format(i,lam[i].size))
+#        Y.append(np.dot(a,np.subtract(k[i],lam[:,i])))
+#    print("Y = {}".format(Y))
     
     return Y# }}}
 
@@ -357,6 +370,23 @@ def loss(y,y_p):# {{{
     '''
     l = [(y[i] - y_p[i])**2 for i in range(len(y))]
     return (sum(l)/len(y))# }}}
+
+def grid_search(tr_x,val_x,tr_y,val_y):# {{{
+    mse = 1E9
+    s_list = np.logspace(-5,8,num=16)
+    l_list = np.logspace(-16,16,num=40,base=-10)
+#    l_list = np.zeros(50)
+#    print("WARNING: Setting all lambda values to zero!")
+    for s in s_list:
+        for l in l_list:
+            a = train(tr_x,tr_y,s,l)
+            y_p = pred(val_x,tr_x,a,s,l)
+            new_mse = abs(loss(val_y,y_p))
+            if new_mse <= mse:
+                s_f = s
+                l_f = l
+                mse = new_mse
+    return s_f, l_f, mse# }}}
 
 def cross_validate(x_data,y_data,k,s=0.05,l=0):# {{{
     '''
@@ -389,6 +419,83 @@ def cross_validate(x_data,y_data,k,s=0.05,l=0):# {{{
         mse_list.append(mse) # save the error
 
     return np.mean(mse_list), y_p_list, mse_list# }}}
+
+def log_space_cv(y_data,x_data,s,l,k=5,step=100,max_it=3000,grad_conv=1E-8):# {{{
+    '''
+    Test function- let's do cross-validation by searching over a log space for
+    my hyperparameters. Optimize a hyperparameter(s) for one fold, push that into the
+    next, optimize, and repeat. 
+    '''
+    print("Cross-validating in {} folds . . .".format(k))
+
+    # split full data into k folds
+    y_data = np.array_split(y_data,k) 
+    x_data = np.array_split(x_data,k) 
+
+    # hold hyperparameters and errors for averaging
+    s_list = []
+    l_list = []
+    mse_list = []
+
+    for val in range(0,k): # loop over validation sets
+        print("Fold {} . . .".format(val+1))
+        tr_y = copy.deepcopy(y_data) # copy full data set
+        tr_x = copy.deepcopy(x_data)
+        val_y = tr_y.pop(val) # pop out the validation set
+        val_x = tr_x.pop(val)
+        tr_y = np.concatenate(tr_y) # re-form training set
+        tr_x = np.concatenate(tr_x) 
+        s, l, mse = grid_search(tr_x,val_x,tr_y,val_y)
+        s_list.append(s)
+        l_list.append(l)
+        mse_list.append(mse)
+        print("s = {}\nl = {}\nmse = {}\n\n".format(s,l,mse))
+#    den = sum(1/mse for mse in mse_list)
+#    s = sum(s_list[i] / mse_list[i] for i in range(len(s_list))) / den
+#    l = sum(l_list[i] / mse_list[i] for i in range(len(l_list))) / den
+    s = np.mean(s_list)
+    l = np.mean(l_list)
+    return s,l # }}}
+
+# old cross-validation w/ gradient descent# {{{
+#        s_new = s+1
+#        fold_error = [] # list of errors for this fold
+#        fold_s = [s,s_new]
+#
+#        for it in range(1,max_it+1):
+##            print("it {}".format(it))
+#            if it == 1:
+#                a = train(tr_x,tr_y,s,l) # train using training set
+#                y_p = pred(val_x,tr_x,a,s,l) # predict validation set
+#                mse = loss(val_y,y_p) # calculate loss
+#                fold_error = np.append(fold_error,mse)
+#            else:
+#                a = train(tr_x,tr_y,s_new,l) # train using training set
+#                y_p = pred(val_x,tr_x,a,s_new,l) # predict validation set
+#                new_mse = loss(val_y,y_p) # calculate loss
+#                fold_error = np.append(fold_error,new_mse) # store error for this fold
+#    
+#                grad = np.gradient(fold_error,fold_s)
+#                abs_grad = [abs(i) for i in grad]
+#        
+#                if min(abs_grad) < grad_conv:
+#                    s = s_new
+#                    print("converged in {} iterations".format(it))
+#                    print("s value: {}".format(s_new))
+#                    print("l value: {}".format(l)) # s-only learning
+#                    #print("CV error: {}".format(cv_error_new))
+#                    break
+#                elif it == max_it:
+#                    s = s_new
+#                    print("too many iterations")
+#                    print("s value: {}".format(s_new))
+#                    print("l value: {}".format(l)) # s-only learning
+#                    break
+#                else:
+#                    s = s_new
+#                    s_new = s - step*grad[-1] # s-only learning
+#                    fold_s.append(s_new)
+#    return s# }}}
 
 if __name__ == "__main__":
     do_krr()
