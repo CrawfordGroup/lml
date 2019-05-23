fc07234bc4ab9db87a135cdf96091f0c0063e3c1
bgpeyton <bgpeyton@vt.edu>
2019-05-23 15:12:46 -0400

    CCSD-KRR working for H2 dissociation

--- mlqm/krr.py
+++ mlqm/krr.py
@@ -8,27 +8,26 @@ import matplotlib.pyplot as plt
 import random
 import copy
 
-def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
+def do_krr(M=12,N=200,st=0.05,K=30,bas="def2-TZVP",save=True):# {{{
     '''
-    kernel ridge regression with M training points and N total representations
+    Kernel Ridge Regression with M training points and N total representations
     st used for tatr generation
-    s used as the stdv for gaussian kernel generation
-    l used for ridge regression
     K repeated k-means algorithms, best clustering chosen
-    '''
-    bas = "def2-TZVP"
-    pes = np.linspace(0.5,2,N) # N evenly-spaced representations on the PES
+    save option to write TATRs and SCF/CCSD energies to file
 
-#    ### TEST N2 TATR #### {{{
-#    geom = """
-#        N
-#        N 1 1.1
-#        symmetry c1
-#        """
-#    mol = mesp.Molecule('N2',geom,bas)
-#    tatr = make_tatr(mol,graph=True)# }}}
+    The algorithm will displace a diatomic over the range 0.5 - 2 Angstroms, generating
+    TATRs and corresponding energies at each point. Then, a training set of `M` TATRs 
+    will be selected by the k-means algorithm. Using this training set, the 
+    hyperparameters `s` and `l` (kernel width and regularization) will be determined by 
+    searching over a coarse logarithmic grid and minimizing the regularized L2 squared 
+    loss cost function. Once the hypers are thus determined, the regression coefficients 
+    `a` are trained by solving the corresponding linear equations. A validation set 
+    spread across the PES (not including any training points) is then predicted using the 
+    model, and the results are graphed.
+    '''
 
     # generate the total data set # {{{
+    pes = np.linspace(0.5,2,N) # N evenly-spaced representations on the PES
     try:
         print("Loading data set . . .")
         tatr_list = np.load('t_list.npy').tolist()
@@ -40,16 +39,15 @@ def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
         E_CCSD_CORR_list = [] # hold CCSD correlation energy
         E_SCF_list = [] # hold SCF energy
         for i in range(0,N):
-            geom = """
-                H
+            mol = psi4.geometry("""
+                H 
                 H 1 """ + str(pes[i]) + """
                 symmetry c1
-            """
-            mol = mesp.Molecule('H2',geom,bas)
-            tatr = make_tatr(mol,150,st)
+            """)
+            tatr, wfn = make_tatr(mol,bas,st=st)
             tatr_list.append(tatr)
-            E_CCSD_CORR_list.append(mol.E_CCSD_CORR)
-            E_SCF_list.append(mol.E_SCF)
+            E_CCSD_CORR_list.append(wfn.variable('CCSD CORRELATION ENERGY'))
+            E_SCF_list.append(wfn.variable('SCF TOTAL ENERGY'))
         if save:
             np.save('t_list.npy',tatr_list)
             np.save('corr_list.npy',E_CCSD_CORR_list)
@@ -59,21 +57,12 @@ def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
     print("Generating training set . . .")
     trainers = gen_train(tatr_list, M, K) 
 
-#    print("Using test training set.")
-#    trainers = sorted([152, 139, 35, 138, 112, 32, 106, 172, 57, 143, 52, 182], reverse=True) 
-    # s=10000,l=0 is good
-
-#    trainers = sorted([98, 116, 178, 47, 8, 138, 182, 32, 157, 75, 144, 143], reverse=True) 
-    # s=100,l=0 is good
-
-#    trainers = [182, 178, 174, 171, 144, 136, 125, 111, 109, 57, 47, 32]
-#    trainers = [184, 180, 176, 172, 144, 136, 124, 112, 108, 56, 48, 32]
-    # the above is completely composed of validation points- should get exact fit at these points
-
     print("Training set: {}".format(trainers))
     t_pos = [pes[i] for i in trainers]
 
     vals = sorted([i for i in range(0,200,4)], reverse=True) # validation set
+    vals = sorted(list(set(vals) - (set(vals) & set(trainers))),reverse=True) # pull trainers out
+    print("Validation set: {}".format(vals))
 
     t_tatr = [] # training TATRs
     t_CORR_E = [] # training energies
@@ -86,7 +75,6 @@ def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
         t_tatr.append(tatr_list[i]) # assuming I don't have to pop the trainers out of set
         t_CORR_E.append(E_CCSD_CORR_list[i])
     for i in vals:
-#        vtatr = tatr_list.pop(i) # make _sure_ to pop vals out of set
         vtatr = tatr_list[i]
         vE = E_CCSD_CORR_list.pop(i)
         v_tatr.append(vtatr)
@@ -97,33 +85,26 @@ def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
     # internally shift all corr E by the avg training set corr E
     t_CORR_avg = np.mean(t_CORR_E)
     t_CORR_E = np.subtract(t_CORR_E,t_CORR_avg)
-    v_CORR_E = np.subtract(v_CORR_E,t_CORR_avg)
-        # }}}
+    v_CORR_E = np.subtract(v_CORR_E,t_CORR_avg)# }}}
 
-    s_new, l = log_space_cv(t_CORR_E,t_tatr,s,l,k=M)
-#     USE BELOW FOR TESTING
-#    print("WARNING: Using static s and l values!")
-#    l = 0
-#    s_new = s 
-#    s_new = 5737.081137
-#    s_new = 11657780
-#    l = 7e-5
-#    l = 7e-26
+    # model determination# {{{
+    # train the hypers
+    s, l = log_space_cv(t_CORR_E,t_tatr,k=M)
 
     # train for alpha
-    print("Model training using s = {} and l = {} . . .".format(s_new,l))
-    alpha = train(t_tatr,t_CORR_E,s_new,l)
+    print("Model training using s = {} and l = {} . . .".format(s,l))
+    alpha = train(t_tatr,t_CORR_E,s,l)# }}}
 
-    # predict E across the PES
+    # predict E across the PES# {{{
     print("Predicting PES . . .")
-    pred_E_list = pred(v_tatr,t_tatr,alpha,s_new,l)
+    pred_E_list = pred(v_tatr,t_tatr,alpha,s,l)
 
-    # plot
     v_E_list = np.add(v_CORR_E,t_CORR_avg)
     v_E_list = np.add(v_E_list,v_SCF)
     pred_E_list = np.add(pred_E_list,t_CORR_avg)
-    pred_E_list = np.add(pred_E_list,v_SCF)
+    pred_E_list = np.add(pred_E_list,v_SCF)# }}}
 
+    # plot# {{{
     plt.figure(1)
     plt.plot(v_PES,v_E_list,'b-o',label='CCSD PES',linewidth=3)
     plt.plot(v_PES,v_SCF,'y-',label='SCF')
@@ -133,7 +114,7 @@ def do_krr(M=12,N=200,st=0.05,s=10000,l=0,K=30,save=True):# {{{
     plt.xlabel('r/Angstrom')
     plt.ylabel('Energy/$E_h$')
     plt.legend()
-    plt.show()
+    plt.show()# }}}
     # }}}
 
 def gaus(x, u, s):# {{{
@@ -143,34 +124,37 @@ def gaus(x, u, s):# {{{
     '''
     return np.exp(-(x-u)**2 / (2.0*s**2))# }}}
 
-def make_tatr(mol,x=150,st=0.05,graph=False):# {{{
+def make_tatr(mol,bas,x=150,st=0.05,graph=False):# {{{
     '''
     make t-amp tensor representation
-    pass in a mesp molecule and (optional) the number of points
+    pass in a psi4 molecule and basis
+    plus (optional) the number of points
     and (optional) the sigma for the gaussian
+    can also graph the 1TATR and 2TATR
     '''
-    mesp.do_ccsd(mol, e_conv=1e-8, save_t=True)
+    psi4.core.clean()
+    psi4.core.clean_options()
+    psi4.core.clean_variables()
+    psi4.set_options({
+        'basis':bas,
+        'scf_type':'pk',
+        'freeze_core':'false',
+        'e_convergence':1e-8,
+        'd_convergence':1e-8})
+    psi4.set_module_options('SCF',
+        {'e_convergence': 1e-8, 'd_convergence': 1e-8,
+         'DIIS': True, 'scf_type':'pk'})
+    e, wfn = psi4.energy('ccsd',molecule=mol,return_wfn=True)
+    amps = wfn.get_amplitudes()
 
-    # if there aren't enough t's, just keep them all
-    # probably redundant...
-#    if len(mol.t1) < x1:
-#        x1 = len(mol.t1.ravel())
-#    if len(mol.t2) < x2:
-#        x2 = len(mol.t2.ravel())
+    # sort amplitudes by magnitude (sorted(x,key=abs) will ignore sign) 
+    t1 = sorted(amps['tIA'].to_array().ravel(),key=abs)[-x:]
+    t2 = sorted(amps['tIjAb'].to_array().ravel(),key=abs)[-x:]
 
-    t1 = np.sort(mol.t1.ravel())[-x:]
-    t2 = np.sort(mol.t2.ravel())[-x:]
-#    t = np.concatenate((t1,t2),axis=None)
-#    print("Total t1 amplitudes: {}".format(len(t1)))
-#    print("Total t2 amplitudes: {}".format(len(t2)))
-#    print("Total t amplitudes: {}".format(len(t)))
-    
     tatr = [] # store eq vals
     tatr1 = [] # singles tatrs
     tatr2 = [] # doubles tatrs
-#    x_list = np.linspace(-1,1,(x1+x2)) # if I get rid of the "redundant" code above, then I need to use the lengths of the t-vector
     x_list = np.linspace(-1,1,x)
-#    print("x_list len: {}".format(len(x_list)))
     for i in range(0,x):
         val1 = 0
         val2 = 0
@@ -180,25 +164,16 @@ def make_tatr(mol,x=150,st=0.05,graph=False):# {{{
             val2 += gaus(x_list[i],t2[t_2],st)
         tatr1.append(val1)
         tatr2.append(val2)
-#        tatr.append(np.concatenate((tatr1,tatr2),axis=None))
-#        tatr.append(np.concatenate((tatr1[i],tatr2[i])))
         tatr.append(val1)
         tatr.append(val2)
-#        print("tatr at x = {}: {}\n".format(x_list[i],tatr[i]))
-#    print("tatr at x = {}: {}\n".format(x_list[149],tatr[149]))
-
-#    for N in range(0,len(t2)):
-#        tatr.append(gaus(x_list[N], t2[N], s))
 
     if graph:
-#        print(tatr)
         plt.figure(1)
         plt.plot(x_list,tatr1,label="singles")
         plt.plot(x_list,tatr2,label="doubles")
         plt.legend()
-#        plt.yscale("log")
         plt.show()
-    return np.asarray(tatr)# }}}
+    return np.asarray(tatr), wfn# }}}
 
 def cluster(pts,M,cens):# {{{
     '''
@@ -242,7 +217,6 @@ def k_means(pts,M):# {{{
     while max(diffs) > 0:
         new_cens = recenter(M,maps)
         maps = cluster(pts,M,new_cens)
-                                                                            
         diffs = [np.mean(cens[i] - new_cens[i]) for i in range(0,len(new_cens))]
         diffs = np.absolute(diffs)
         cens = new_cens
@@ -349,82 +323,40 @@ def pred(x,xt,a,s=0.05,l=0):# {{{
 
     # predict energy for the entire PES
     Y = [] # 
-#    lam = l*np.eye(N)[:M,:]
-#    lam = lam[:len(k[0])]
     for i in range(0,N):
-#        print("a is {} long\nk[{}] is {} long".format(len(a),i,len(k[i])))
         Y.append(np.dot(a,k[i]))
-#        print("k[{}] = {}".format(i,k[i]))
-#        print("lam = {}".format(lam))
-#        print("lam is {}x{}".format(len(lam),len(lam)))
-#        print("lam[{}] is size {}".format(i,lam[i].size))
-#        Y.append(np.dot(a,np.subtract(k[i],lam[:,i])))
-#    print("Y = {}".format(Y))
     
     return Y# }}}
 
-def loss(y,y_p):# {{{
+def loss(y,y_p,l,a):# {{{
     '''
-    Calculate L2 (squared) loss, return mean squared error
-    given true and predicted values
+    Calculate L2 (squared) loss with regularization to protect against
+    large norm squared regression coefficients
+    given true and predicted values, regularization, and regression coefficients
     '''
-    l = [(y[i] - y_p[i])**2 for i in range(len(y))]
-    return (sum(l)/len(y))# }}}
+    ls = sum([(y_p[i] - y[i])**2 for i in range(len(y))]) + l*np.linalg.norm(a)**2
+    return ls# }}}
 
 def grid_search(tr_x,val_x,tr_y,val_y):# {{{
     mse = 1E9
     s_list = np.logspace(-5,8,num=16)
-    l_list = np.logspace(-16,16,num=40,base=-10)
-#    l_list = np.zeros(50)
-#    print("WARNING: Setting all lambda values to zero!")
+    l_list = np.logspace(-8,-1,num=16)
     for s in s_list:
         for l in l_list:
             a = train(tr_x,tr_y,s,l)
             y_p = pred(val_x,tr_x,a,s,l)
-            new_mse = abs(loss(val_y,y_p))
+            new_mse = abs(loss(val_y,y_p,l,a))
             if new_mse <= mse:
                 s_f = s
                 l_f = l
                 mse = new_mse
     return s_f, l_f, mse# }}}
 
-def cross_validate(x_data,y_data,k,s=0.05,l=0):# {{{
+def log_space_cv(y_data,x_data,k=5):# {{{
     '''
-    k-fold cross validation: k folds, one is kept for validation
-    "rotate" folds, re-validate, repeat to finish
-    given x and y values to regress, number of folds k
-    s and l hyperparameters (kernel width and regularization)
-    return average error and predicted values + their errors for each fold 
-    may also want to return the parameter (a)...
-    '''
-    # split full data into k folds
-    y_data = np.array_split(y_data,k) 
-    x_data = np.array_split(x_data,k) 
-
-    # pop out validation set, train/validate, repeat
-    y_p_list = [] # predicted values
-    mse_list = [] # mean squared errors
-    for val in range(0,k): # loop over validation sets
-        tr_y = copy.deepcopy(y_data) # copy full data set
-        tr_x = copy.deepcopy(x_data)
-        val_y = tr_y.pop(val) # pop out the validation set
-        val_x = tr_x.pop(val)
-        tr_y = np.concatenate(tr_y) # re-form training set
-        tr_x = np.concatenate(tr_x) 
-
-        a = train(tr_x,tr_y,s,l) # train using training set
-        y_p = pred(val_x,tr_x,a,s,l) # predict validation set
-        mse = loss(val_y,y_p) # calculate loss
-        y_p_list.append(y_p) # save the answers
-        mse_list.append(mse) # save the error
-
-    return np.mean(mse_list), y_p_list, mse_list# }}}
-
-def log_space_cv(y_data,x_data,s,l,k=5,step=100,max_it=3000,grad_conv=1E-8):# {{{
-    '''
-    Test function- let's do cross-validation by searching over a log space for
-    my hyperparameters. Optimize a hyperparameter(s) for one fold, push that into the
-    next, optimize, and repeat. 
+    Cross-validation by searching over a log space for hyperparameters. 
+    Optimize hyperparameter(s) for one fold, push that into the next, 
+    optimize, and repeat. 
     '''
     print("Cross-validating in {} folds . . .".format(k))
 
@@ -450,52 +382,9 @@ def log_space_cv(y_data,x_data,s,l,k=5,step=100,max_it=3000,grad_conv=1E-8):# {{
         l_list.append(l)
         mse_list.append(mse)
         print("s = {}\nl = {}\nmse = {}\n\n".format(s,l,mse))
-#    den = sum(1/mse for mse in mse_list)
-#    s = sum(s_list[i] / mse_list[i] for i in range(len(s_list))) / den
-#    l = sum(l_list[i] / mse_list[i] for i in range(len(l_list))) / den
     s = np.mean(s_list)
     l = np.mean(l_list)
     return s,l # }}}
-
-# old cross-validation w/ gradient descent# {{{
-#        s_new = s+1
-#        fold_error = [] # list of errors for this fold
-#        fold_s = [s,s_new]
-#
-#        for it in range(1,max_it+1):
-##            print("it {}".format(it))
-#            if it == 1:
-#                a = train(tr_x,tr_y,s,l) # train using training set
-#                y_p = pred(val_x,tr_x,a,s,l) # predict validation set
-#                mse = loss(val_y,y_p) # calculate loss
-#                fold_error = np.append(fold_error,mse)
-#            else:
-#                a = train(tr_x,tr_y,s_new,l) # train using training set
-#                y_p = pred(val_x,tr_x,a,s_new,l) # predict validation set
-#                new_mse = loss(val_y,y_p) # calculate loss
-#                fold_error = np.append(fold_error,new_mse) # store error for this fold
-#    
-#                grad = np.gradient(fold_error,fold_s)
-#                abs_grad = [abs(i) for i in grad]
-#        
-#                if min(abs_grad) < grad_conv:
-#                    s = s_new
-#                    print("converged in {} iterations".format(it))
-#                    print("s value: {}".format(s_new))
-#                    print("l value: {}".format(l)) # s-only learning
-#                    #print("CV error: {}".format(cv_error_new))
-#                    break
-#                elif it == max_it:
-#                    s = s_new
-#                    print("too many iterations")
-#                    print("s value: {}".format(s_new))
-#                    print("l value: {}".format(l)) # s-only learning
-#                    break
-#                else:
-#                    s = s_new
-#                    s_new = s - step*grad[-1] # s-only learning
-#                    fold_s.append(s_new)
-#    return s# }}}
 
 if __name__ == "__main__":
     do_krr()
