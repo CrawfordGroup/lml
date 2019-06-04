2dfbd31c0ba36ca18696173ca4e4d7668a59df91
bgpeyton <bgpeyton@vt.edu>
2019-06-04 18:46:27 -0400

    ML-PT now working, input json scheme updated. Example updated.

--- mlqm/krr/krr.py
+++ mlqm/krr/krr.py
@@ -13,12 +13,16 @@ def do_krr(inpf,save=True):# {{{
     inpf must contain AT LEAST:
     inp['mol']['geom']: a geometry with a variable (gvar) given for displacement along PES
     inp['setup']: 'M', 'N', 'st', 'K', 'gvar' (list), 'grange' (list), 'basis'
+    and empty/'false' spots for several to-be-filled fields.
     See examples at github.com/bgpeyton/lml/mlqm/krr/examples/
 
     Kernel Ridge Regression with M training points and N total representations
+    `valtype` is used for validation and high-level training data (IE, energies)
+    `predtype` is used for representations and predictions (IE, TATRs)
+    in short: "Predict `valtype`-level energies using `predtype`-level TATRs"
     st used for tatr generation
     K repeated k-means algorithms, best clustering chosen
-    save option to write TATRs and SCF/CCSD energies to file
+    save option to write TATRs and  energies to file
 
     The algorithm will displace a diatomic over `grange[0]-grange[1]` Angstroms, generating
     TATRs and corresponding energies at each point. Then, a training set of `M` TATRs 
@@ -29,6 +33,9 @@ def do_krr(inpf,save=True):# {{{
     `a` are trained by solving the corresponding linear equations. A validation set 
     spread across the PES (not including any training points) is then predicted using the 
     model, and the results are graphed.
+
+    Use from command line as:
+    >>>python krr.py inpf
     '''
     # parse input# {{{
     with open(inpf,'r') as f:
@@ -38,42 +45,56 @@ def do_krr(inpf,save=True):# {{{
     st   = inp['setup']['st']
     K    = inp['setup']['K']
     bas  = inp['setup']['basis']
-    geom = inp['mol']['geom']# }}}
+    geom = inp['mol']['geom']
+    valtype  = inp['setup']['valtype'].upper() # "high" theory for the training set
+    predtype = inp['setup']['predtype'].upper() # "low" theory for the predictions
+    ref = inp['setup']['ref'] # whether or not to store and graph validation set with val and predtype
+    print("Using {} amplitudes to predict {}-level energies!".format(predtype,valtype))
+    # }}}
 
-    # generate the total data set # {{{
+    # generate the grand data set representations # {{{
     # N evenly-spaced representations on the PES
     bot = float(inp['setup']['grange'][0])
     top = float(inp['setup']['grange'][1])
     gvar = inp['setup']['gvar'][0]
     pes = np.linspace(bot,top,N) 
-    while inp['data']['generated']:
+
+    while inp['data']['grand_generated']:
         try:
             print("Loading data set . . .")
             tatr_list = np.load('t_list.npy').tolist()
-            E_CCSD_CORR_list = np.load('corr_list.npy').tolist()
             E_SCF_list = np.load('scf_list.npy').tolist()
+            if (predtype == valtype) or (ref == True):
+                g_E_CORR_list = np.load('grand_corr_list.npy').tolist() 
+                # grand low-level correlation energy needed if pred=val, or if you want
+                # the reference (low-level) energies to graph
             break
         except FileNotFoundError:
             print("Data not found. Proceeding to data generation.")
-            inp['data']['generated'] = False
-    else:
-        print("Generating data set . . .")
-        tatr_list = [] # hold TATRs
-        E_CCSD_CORR_list = [] # hold CCSD correlation energy
-        E_SCF_list = [] # hold SCF energy
+            inp['data']['grand_generated'] = False
 
-        for i in range(0,N):
-            new_geom = geom.replace(gvar,str(pes[i]))
-            mol = psi4.geometry(new_geom)
-            tatr, wfn = make_tatr(mol,bas,st=st)
-            tatr_list.append(tatr)
-            E_CCSD_CORR_list.append(wfn.variable('CCSD CORRELATION ENERGY'))
-            E_SCF_list.append(wfn.variable('SCF TOTAL ENERGY'))
-        inp['data']['generated'] = True
-        if save:
-            np.save('t_list.npy',tatr_list)
-            np.save('corr_list.npy',E_CCSD_CORR_list)
-            np.save('scf_list.npy',E_SCF_list)
+    else:
+        if predtype in ["MP2","CCSD"]:
+            print("Generating grand {} data set . . .".format(predtype))
+            tatr_list = [] # hold TATRs
+            g_E_CORR_list = [] # hold low-level correlation energy for grand data set
+            E_SCF_list = [] # hold SCF energy
+            for i in range(0,N):
+                new_geom = geom.replace(gvar,str(pes[i]))
+                mol = psi4.geometry(new_geom)
+                tatr, wfn = make_tatr(mol,predtype,bas,st=st)
+                tatr_list.append(tatr)
+                g_E_CORR_list.append(wfn.variable('{} CORRELATION ENERGY'.format(predtype)))
+                E_SCF_list.append(wfn.variable('SCF TOTAL ENERGY'))
+            inp['data']['grand_generated'] = True
+            if save:
+                np.save('t_list.npy',tatr_list)
+                np.save('grand_corr_list.npy',g_E_CORR_list)
+                np.save('scf_list.npy',E_SCF_list)
+        else:
+            print("Prediction set type {} not supported.".format(predtype))
+            raise Exception("Prediction set type {} not supported!".format(predtype))
+
     # update the input
     with open(inpf,'w') as f:
         json.dump(inp, f, indent=4)# }}}
@@ -92,11 +113,13 @@ def do_krr(inpf,save=True):# {{{
         print("Generating training set . . .")
         trainers = gen_train(tatr_list, M, K) 
         inp['data']['trainers'] = trainers
-        with open(inpf,'w') as f:
-            json.dump(inp, f, indent=4)
+
+    # update the input
+    with open(inpf,'w') as f:
+        json.dump(inp, f, indent=4)
 
     print("Training set: {}".format(trainers))
-    t_pos = [pes[i] for i in trainers]
+    t_pos = [pes[i] for i in trainers] # training set positions for graphing purposes
 
     vals = sorted([i for i in range(0,200,4)], reverse=True) # validation set
     vals = sorted(list(set(vals) - (set(vals) & set(trainers))),reverse=True) # pull trainers out
@@ -105,25 +128,100 @@ def do_krr(inpf,save=True):# {{{
     t_tatr = [] # training TATRs
     t_CORR_E = [] # training energies
     v_tatr = [] # validation TATRs
-    v_CORR_E = [] # validation energies
+    v_CORR_E = [] # validation energies (for graphing with ref=True)
+    p_CORR_E = [] # validation energies with predtype (for graphing with ref=True)
     v_SCF = [] # validation SCF energies
     v_PES = [] # validation PES
 
-    for i in trainers:
-        t_tatr.append(tatr_list[i]) # assuming I don't have to pop the trainers out of set
-        t_CORR_E.append(E_CCSD_CORR_list[i])
-    for i in vals:
-        vtatr = tatr_list[i]
-        vE = E_CCSD_CORR_list.pop(i)
-        v_tatr.append(vtatr)
-        v_CORR_E.append(vE)
-        v_SCF.append(E_SCF_list[i])
-        v_PES.append(pes[i])
+    if valtype == predtype:
+        print("Sorting training/validation sets from existing data.")
+        for i in trainers:
+            t_tatr.append(tatr_list[i]) # assuming I don't have to pop the trainers out of set
+            t_CORR_E.append(E_CORR_list[i])
+        for i in vals:
+            vtatr = tatr_list[i]
+            vE = E_CORR_list.pop(i)
+            v_tatr.append(vtatr)
+            v_CORR_E.append(vE)
+            v_SCF.append(E_SCF_list[i])
+            v_PES.append(pes[i])
+    else:
+        # for MP2->CCSD, we need MP2 TATRs (already computed) and CCSD energies
+        # for training set and validation set
+        # NOTE: while we need validation set TATRs for prediction, the energies
+        # are used solely for the purpose of graphing the "true" PES against
+        # the ML-PES. Thus in a "real" scenario, additional calculations on the
+        # validation set will not be done.
+        while inp['data']['train_generated']:
+            print("Attempting to load training data from file . . .")
+            try:
+                t_CORR_E = np.load('train_corr_list.npy').tolist()
+                for i in trainers:
+                    t_tatr.append(tatr_list[i])
+                break
+            except FileNotFoundError:
+                print("Data not found. Proceeding to data generation.")
+                inp['data']['train_generated'] = False
+        else:
+            print("Generating training set data . . .")
+            psi4.core.clean()
+            psi4.core.clean_options()
+            psi4.core.clean_variables()
+            psi4.set_options({"basis":bas})
+            for i in trainers:
+                t_tatr.append(tatr_list[i])
+                new_geom = geom.replace(gvar,str(pes[i]))
+                mol = psi4.geometry(new_geom)
+                _, wfn = psi4.energy(valtype,return_wfn=True)
+                t_CORR_E.append(wfn.variable('{} CORRELATION ENERGY'.format(valtype)))
+            np.save('train_corr_list.npy',t_CORR_E)
+            inp['data']['train_generated'] = True
+
+        # update the input
+        with open(inpf,'w') as f:
+            json.dump(inp, f, indent=4)
+
+        while inp['data']['valid_generated']:
+            print("Attempting to load validation data from file . . .")
+            try:
+                if ref == True:
+                    p_CORR_E.append(g_E_CORR_list[i])
+                    v_CORR_E = np.load('valid_corr_list.npy').tolist()
+                for i in vals:
+                    v_tatr.append(tatr_list[i])
+                    v_SCF.append(E_SCF_list[i])
+                    v_PES.append(pes[i])
+                break
+            except FileNotFoundError:
+                print("Data not found. Proceeding to data generation.")
+                inp['data']['grand_generated'] = False
+        else:
+            print("Generating validation set data . . .")
+            for i in vals:
+                v_tatr.append(tatr_list[i])
+                v_SCF.append(E_SCF_list[i])
+                v_PES.append(pes[i])
+                if ref == True:
+                    p_CORR_E.append(g_E_CORR_list[i])
+                    new_geom = geom.replace(gvar,str(pes[i]))
+                    mol = psi4.geometry(new_geom)
+                    _, wfn = psi4.energy(valtype,return_wfn=True)
+                    v_CORR_E.append(wfn.variable('{} CORRELATION ENERGY'.format(valtype)))
+            if ref == True:
+                np.save('valid_corr_list.npy',v_CORR_E)
+            inp['data']['valid_generated'] = True
+
+        # update the input
+        with open(inpf,'w') as f:
+            json.dump(inp, f, indent=4)
 
     # internally shift all corr E by the avg training set corr E
     t_CORR_avg = np.mean(t_CORR_E)
     t_CORR_E = np.subtract(t_CORR_E,t_CORR_avg)
-    v_CORR_E = np.subtract(v_CORR_E,t_CORR_avg)# }}}
+    if ref == True:
+        v_CORR_E = np.subtract(v_CORR_E,t_CORR_avg)
+        p_CORR_E = np.subtract(p_CORR_E,t_CORR_avg)
+        # }}}
 
     # model determination# {{{
     # train the hypers
@@ -155,10 +253,14 @@ def do_krr(inpf,save=True):# {{{
     print("Predicting PES . . .")
     pred_E_list = pred(v_tatr,t_tatr,alpha,s,l)
 
-    v_E_list = np.add(v_CORR_E,t_CORR_avg)
-    v_E_list = np.add(v_E_list,v_SCF)
     pred_E_list = np.add(pred_E_list,t_CORR_avg)
-    pred_E_list = np.add(pred_E_list,v_SCF)# }}}
+    pred_E_list = np.add(pred_E_list,v_SCF)
+    if ref == True:
+        v_E_list = np.add(v_CORR_E,t_CORR_avg)
+        v_E_list = np.add(v_E_list,v_SCF)
+        p_E_list = np.add(p_CORR_E,t_CORR_avg)
+        p_E_list = np.add(p_E_list,v_SCF)
+        # }}}
 
     # plot# {{{
     if inp['setup']['plot']:
@@ -166,9 +268,11 @@ def do_krr(inpf,save=True):# {{{
             plt.figure(1,dpi=inp['setup']['dpi'])
         else:
             plt.figure(1,dpi=100)
-        plt.plot(v_PES,v_E_list,'b-o',label='CCSD PES',linewidth=3)
         plt.plot(v_PES,v_SCF,'y-',label='SCF')
-        plt.plot(v_PES,pred_E_list,'r-^',ms=2,label='CCSD/ML-{}'.format(M),linewidth=2)
+        if ref == True:
+            plt.plot(v_PES,p_E_list,'r--',label='{} PES'.format(predtype))
+            plt.plot(v_PES,v_E_list,'b-o',label='{} PES'.format(valtype),linewidth=3)
+        plt.plot(v_PES,pred_E_list,'r-^',ms=2,label='{}/ML-{}'.format(predtype,M),linewidth=2)
         plt.plot(t_pos,[-1.18 for i in range(0,len(t_pos))],'go',label='Training points')
         if inp['setup']['axis']:
             plt.axis(inp['setup']['axis'])
@@ -188,13 +292,13 @@ def gaus(x, u, s):# {{{
     '''
     return np.exp(-(x-u)**2 / (2.0*s**2))# }}}
 
-def make_tatr(mol,bas,x=150,st=0.05,graph=False):# {{{
+def make_tatr(mol,theory,bas,x=150,st=0.05,graph=False):# {{{
     '''
     make t-amp tensor representation
-    pass in a psi4 molecule and basis
+    pass in a psi4 molecule with theory and basis
     plus (optional) the number of points
     and (optional) the sigma for the gaussian
-    can also graph the 1TATR and 2TATR
+    can also graph the representations
     '''
     psi4.core.clean()
     psi4.core.clean_options()
@@ -205,38 +309,88 @@ def make_tatr(mol,bas,x=150,st=0.05,graph=False):# {{{
         'freeze_core':'false',
         'e_convergence':1e-8,
         'd_convergence':1e-8})
-    psi4.set_module_options('SCF',
-        {'e_convergence': 1e-8, 'd_convergence': 1e-8,
-         'DIIS': True, 'scf_type':'pk'})
-    e, wfn = psi4.energy('ccsd',molecule=mol,return_wfn=True)
-    amps = wfn.get_amplitudes()
 
-    # sort amplitudes by magnitude (sorted(x,key=abs) will ignore sign) 
-    t1 = sorted(amps['tIA'].to_array().ravel(),key=abs)[-x:]
-    t2 = sorted(amps['tIjAb'].to_array().ravel(),key=abs)[-x:]
+    if theory == "CCSD":# {{{
+        # compute and grab amplitudes
+        e, wfn = psi4.energy("CCSD",molecule=mol,return_wfn=True)
+        amps = wfn.get_amplitudes()
 
-    tatr = [] # store eq vals
-    tatr1 = [] # singles tatrs
-    tatr2 = [] # doubles tatrs
-    x_list = np.linspace(-1,1,x)
-    for i in range(0,x):
-        val1 = 0
-        val2 = 0
-        for t_1 in range(0,len(t1)):
-            val1 += gaus(x_list[i],t1[t_1],st)
-        for t_2 in range(0,len(t2)):
-            val2 += gaus(x_list[i],t2[t_2],st)
-        tatr1.append(val1)
-        tatr2.append(val2)
-        tatr.append(val1)
-        tatr.append(val2)
+        # sort amplitudes by magnitude (sorted(x,key=abs) will ignore sign) 
+        t1 = sorted(amps['tIA'].to_array().ravel(),key=abs)[-x:]
+        t2 = sorted(amps['tIjAb'].to_array().ravel(),key=abs)[-x:]
 
-    if graph:
-        plt.figure(1)
-        plt.plot(x_list,tatr1,label="singles")
-        plt.plot(x_list,tatr2,label="doubles")
-        plt.legend()
-        plt.show()
+        # make a discretized gaussian using the amps
+        tatr = [] # store eq vals
+        tatr1 = [] # singles tatrs
+        tatr2 = [] # doubles tatrs
+        x_list = np.linspace(-1,1,x)
+        for i in range(0,x):
+            val1 = 0
+            val2 = 0
+            for t_1 in range(0,len(t1)):
+                val1 += gaus(x_list[i],t1[t_1],st)
+            for t_2 in range(0,len(t2)):
+                val2 += gaus(x_list[i],t2[t_2],st)
+            tatr1.append(val1)
+            tatr2.append(val2)
+            tatr.append(val1)
+            tatr.append(val2)# }}}
+
+    elif theory == "MP2":# {{{
+        # no python access to MP2 amps, compute them by hand
+        # see spin-orbital CCSD code in Psi4Numpy
+        e, wfn = psi4.energy("MP2",molecule=mol,return_wfn=True)
+        mints = psi4.core.MintsHelper(wfn.basisset())
+        nocc = wfn.doccpi()[0] * 2
+        nvirt = wfn.nmo()*2 - nocc
+        MO = np.asarray(mints.mo_spin_eri(wfn.Ca(), wfn.Ca()))
+        o = slice(0, nocc)
+        v = slice(nocc, MO.shape[0])
+        H = np.asarray(mints.ao_kinetic()) + np.asarray(mints.ao_potential())
+        H = np.einsum('uj,vi,uv', wfn.Ca(), wfn.Ca(), H)
+        H = np.repeat(H, 2, axis=0)
+        H = np.repeat(H, 2, axis=1)
+        spin_ind = np.arange(H.shape[0], dtype=np.int) % 2
+        H *= (spin_ind.reshape(-1, 1) == spin_ind)
+        MOijab = MO[o, o, v, v]
+        F = H + np.einsum('pmqm->pq', MO[:, o, :, o])
+        Focc = F[np.arange(nocc), np.arange(nocc)].flatten()
+        Fvirt = F[np.arange(nocc, nvirt + nocc), np.arange(nocc, nvirt + nocc)].flatten()
+        Dijab = Focc.reshape(-1, 1, 1, 1) + Focc.reshape(-1, 1, 1) - Fvirt.reshape(-1, 1) - Fvirt
+        t2_full = MOijab / Dijab
+
+        # sort amplitudes by magnitude (sorted(x,key=abs) will ignore sign) 
+        t2 = sorted(t2_full.ravel(),key=abs)[-x:]
+
+        # make a discretized gaussian using the amps
+        tatr = [] # store eq vals
+        x_list = np.linspace(-1,1,x)
+        for i in range(0,x):
+            val2 = 0
+            for t_2 in range(0,len(t2)):
+                val2 += gaus(x_list[i],t2[t_2],st)
+            tatr.append(val2)# }}}
+
+    else: 
+        print("I don't know how to handle {} amplitudes.".format(theory))
+        raise Exception("{} amplitude representations not supported!")
+
+    if graph:# {{{
+        if theory == "CCSD":
+            plt.figure(1)
+            plt.plot(x_list,tatr1,label="singles")
+            plt.plot(x_list,tatr2,label="doubles")
+            plt.legend()
+            plt.show()
+        elif theory == "MP2":
+            plt.figure(1)
+            plt.plot(x_list,tatr1,label="MP2 TATRs")
+            plt.legend()
+            plt.show()
+        else: 
+            print("I don't know how to graph {} amplitudes.".format(theory))
+            raise Exception("Cannot graph this!")# }}}
+
     return np.asarray(tatr), wfn# }}}
 
 def cluster(pts,M,cens):# {{{
