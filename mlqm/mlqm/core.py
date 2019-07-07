6933d4815ee0ca992b825c5a7c26b7b7bf4440cb
bgpeyton <bgpeyton@vt.edu>
2019-07-07 01:57:16 -0400

    Datahelper: write psi4 inputs, runner, and grabber functions. PES class implemented with input file generation and running functions.

--- mlqm/mlqm/core.py
+++ mlqm/mlqm/core.py
@@ -7,6 +7,138 @@ import json
 from . import train
 from . import datahelper
 
+class PES(object):
+    """
+    A class for holding the information necessary for generating a potential 
+    energy surface. Base geometries, displacements, etc. are included here.
+    """
+
+    def __init__(self,inp):
+    # {{{
+        if isinstance(inp,str): # if str, unpack it as a json file
+            with open(inp) as f:
+                inpf = json.load(f)
+            self.inpf = inp
+            if 'name' in inpf:
+                self.name = inpf['name'] # Name of PES 
+            else:
+                self.name = 'PES'
+            self.geom = inpf['geom'] # Base geometry
+            self.gvar = inpf['gvar'] # Variable name in geometry
+            self.dis = inpf['dis'] # Displacement range of geometry
+            self.pts = inpf['pts'] # Number of points on PES
+            self.method = inpf['method']
+            self.basis = inpf['basis']
+            self.generated = inpf['generated']
+            self.complete = inpf['complete']
+            print("PES generated from {}.".format(inp))
+        elif isinstance(inp,dict): # if a dict, unpack it to class variables
+            self.inpf = None
+            if 'name' in inp:
+                self.name = inp['name'] # Name of PES 
+            else:
+                self.name = 'PES'
+            self.geom = inp['geom'] # Base geometry
+            self.gvar = inp['gvar'] # Variable name in geometry
+            self.dis = inp['dis'] # Displacement range of geometry
+            self.pts = inp['pts'] # Number of points on PES
+            self.method = inp['method']
+            self.basis = inp['basis']
+            self.generated = inp['generated']
+            self.complete = inp['complete']
+            print("PES generated from input dictionary.")
+        else: # initialize an empty database
+            self.inpf = None 
+            self.name = None
+            self.geom = None
+            self.gvar = None
+            self.dis = None
+            self.pts = None
+            self.method = None
+            self.basis = None
+            self.generated = False
+            self.complete = False
+            print("Empty PES generated. Please supply variables.")
+        self.dirlist = None # directory list, may be generated or pushed in later
+    # }}}
+
+    def generate(self, global_options=False, regen=False,**kwargs):
+        # {{{
+        '''
+        Generate the input files across a PES.
+        Require the method and psi4.set_options dictionary.
+        Pass kwargs forward into the input file generation.
+        '''
+        # generate the grand data set representations
+        # N evenly-spaced representations on the PES
+        bot = float(self.dis[0])
+        top = float(self.dis[1])
+        base = self.geom
+        gvar = self.gvar
+        pes = np.linspace(bot,top,self.pts) 
+    
+        if not global_options:
+            global_options = {
+            'basis':self.basis,
+            'scf_type':'pk',
+            'mp2_type':'conv',
+            'freeze_core':'false',
+            'e_convergence':1e-8,
+            'd_convergence':1e-8}
+    
+        if 'directory' in kwargs:
+            bdir = kwargs['directory']
+        else:
+            bdir = '.'
+                
+        if (self.generated == True) and (regen == False):
+            dlist = [(bdir + '/{}'.format(dis)) for dis in pes]
+            self.dirlist = dlist
+            print('PES input files already generated! Pass `regen=True` '
+                  ' to regenerate.')
+            return dlist
+        
+        dlist = [] # list of directories
+    
+        for dis in pes:
+            ndir = bdir + '/{}'.format(dis)
+            dlist.append(ndir)
+            kwargs['directory'] = ndir
+            new_geom = base.replace(gvar,str(dis))
+            datahelper.write_psi4_input(new_geom,self.method,global_options,**kwargs)
+
+        self.dirlist = dlist
+    
+        print('PES input files generated! Please run them using the given directory '
+              'list. Once complete, you may parse them with pes.parse().')
+    
+        return dlist
+        # }}}
+
+    def run(self,infile='input.dat',restart=False):
+        # {{{
+        '''
+        Run all jobs listed in self.dirlist (may be set manually or by PES.generate())
+        NOTE: This simply runs all jobs one-by-one. This is not recommended for large
+        datasets, as you are better served running the generated input files
+        in parallel. Use at your own risk!
+        NOTE: This function could be swapped for a more robust system which keeps 
+        track of whether or not jobs have been run, can restart where it left off,
+        or other such fancy things. But for now, this is what I'm doing.
+        '''
+        if self.dirlist == None:
+            print('Please pass a directory list into PES.dirlist or have it set '
+                  'by running PES.generate().')
+            return 0
+        elif (self.complete == True) and (restart == False):
+            print('PES jobs already complete! Pass `restart=True` to rerun.')
+            return 0
+        else:
+            datahelper.runner(self.dirlist,infile=infile)
+            self.complete = True
+            return 0
+        # }}}
+
 class Dataset(object):
     """
     The main data class for training, validation, and testing representations.
@@ -18,6 +150,7 @@ class Dataset(object):
     """
 
     def __init__(self, inpf=None, loc_str=None):
+    # {{{
         """
         Initialize the dataset. Pass in a data set or the location of one.
         """
@@ -48,15 +181,15 @@ class Dataset(object):
         if loc_str is not None:
             if isinstance(loc_str, str):
                 try:
-                    self.grand = np.load(loc_str).to_list()
+                    self.grand = np.load(loc_str)
                     print("Data loaded from file.")
                 except FileNotFoundError:
                     raise RuntimeError("Data could not be loaded from file: {}".format(loc_str))
             elif isinstance(loc_str,np.ndarray):
-                self.data = loc_str
+                self.grand = loc_str
                 print("Data loaded.")
             elif isinstance(loc_str,list):
-                self.data = np.asarray(loc_str)
+                self.grand = np.asarray(loc_str)
                 print("Data loaded.")
             else:
                 raise RuntimeError("""Data type {} not recognized. Please provide string 
@@ -64,25 +197,30 @@ class Dataset(object):
                         data.""".format(type(loc_str)))
         else:
             print("Empty Dataset loaded.")
+    # }}}
 
     def load(self, loc_str):
+    # {{{
         if isinstance(loc_str, str):
             try:
-                self.grand = np.load(loc_str).to_list()
+                self.grand = np.load(loc_str)
                 print("Data loaded from file.")
             except FileNotFoundError:
                 raise RuntimeError("Data could not be loaded from file: {}".format(loc_str))
         elif isinstance(loc_str,np.ndarray):
-            self.data = loc_str
+            self.grand = loc_str
             print("Data loaded.")
-
-    def gen_grand(self, gen_type, **kwargs):
-        if gen_type.lower() in ["pes"]:
-            self.grand["representations"], self.grand["values"], self.grand["reference"] = datahelper.pes_gen(self, **kwargs)
+        elif isinstance(loc_str,list):
+            self.grand = np.asarray(loc_str)
+            print("Data loaded.")
         else:
-            print("Generation of {} data not supported.".format(gen_type))
+            raise RuntimeError("""Data type {} not recognized. Please provide string 
+                    location of numpy file, numpy.ndarray data, or list 
+                    data.""".format(type(loc_str)))
+    # }}}
 
     def find_trainers(self, traintype, **kwargs):
+     # {{{
         if "remove" in kwargs:
             print("NOTE: Trainer map will be valid for grand data set once removed points are dropped!")
         if traintype.lower() in ["kmeans","k-means","k_means"]:
@@ -107,4 +245,16 @@ class Dataset(object):
                 return sorted(trainers, reverse=True)
         else:
             raise RuntimeError("I don't know how to get {} training points yet!".format(traintype))
+    # }}}
+
+    def gen_grand(self, gen_type, **kwargs):
+    # {{{
+        '''
+        DEPRECATION WARNING: moving data generation out of the dataset class!
+        '''
+        if gen_type.lower() in ["pes"]:
+            self.grand["representations"], self.grand["values"], self.grand["reference"] = datahelper.pes_gen(self, **kwargs)
+        else:
+            print("Generation of {} data not supported.".format(gen_type))
+    # }}}
 
--- mlqm/mlqm/datahelper.py
+++ mlqm/mlqm/datahelper.py
@@ -2,9 +2,159 @@ import psi4
 psi4.core.be_quiet()
 import json
 import numpy as np
+import os
+import subprocess
 from . import repgen
 
-def pes_gen(ds, **kwargs):
+def write_psi4_input(molstr,method,global_options,**kwargs):
+    # {{{
+    '''
+    Pass in a molecule string, method, and global options dictionary
+    Kwargs to hold optional directory, module options dictionary,
+    alternative calls (ie properties) and extra commands as strings 
+    Writes a PsiAPI python input file to directory/input.dat
+    '''
+    if 'call' in kwargs:
+        call = kwargs['call'] + '\n\n'
+    else:
+        call = 'e, wfn = psi4.energy("{}",return_wfn=True)'.format(method)
+
+    if 'directory' in kwargs:
+        directory = kwargs['directory']
+        try:
+            os.makedirs(directory)
+        except:
+            if os.path.isdir(directory):
+                pass
+            else:
+                raise Exception('Attempt to create {} directory '
+                                'failed.'.format(directory))
+    else:
+        directory = '.'
+
+    if 'module_options' in kwargs:
+        module_options = kwargs['module_options']
+    else:
+        module_options = False
+
+    if 'extra' in kwargs: 
+        extra = kwargs['extra']
+    else:
+        extra = False
+
+    infile = open('{}/input.dat'.format(directory),'w')
+    infile.write('# This is a psi4 input file auto-generated for MLQM.\n')
+    infile.write('import json\n')
+    infile.write('import psi4\n')
+    infile.write('psi4.core.set_output_file("output.dat")\n\n')
+    infile.write('mol = psi4.geometry("""\n{}\n""")\n\n'.format(molstr))
+    infile.write('psi4.set_options(\n{}\n)\n\n'.format(global_options))
+    if module_options:
+        infile.write('psi4.set_module_options(\n{}\n)\n\n'.format(module_options))
+    infile.write('{}\n\n'.format(call))
+    infile.write('with open("output.json","w") as dumpf:\n'
+                '   json.dump(wfn.variables(), dumpf, indent=4)\n\n')
+    if extra:
+        infile.write('{}'.format(extra))
+    # }}}
+
+def runner(dlist,infile='input.dat'):
+    # {{{
+    '''
+    Run every input in the directory list
+    Default input file recommended
+    '''
+    wd = os.getcwd()
+    for d in dlist:
+        os.chdir(d)
+        subprocess.call(['psi4', 'input.dat', 'output.dat']) 
+        os.chdir(wd)
+    # }}}
+
+def grabber(dlist,fnames=False,varnames=False,outfile='output.json'):
+    # {{{
+    '''
+    Take in a list of directories and either filenames or variable names
+    Filenames should correspond to numpy files to load and return
+    Variable names should correspond to variables in a Psi4 JSON output
+    Default Psi4 JSON output name is recommended
+    Return a result dictionary
+    '''
+    rdict = {}
+
+    # Harvesting each result separately for now. Worse for IO, but better for 
+    # result dictionary structure. 
+    if fnames:
+        for fname in fnames:
+            rdict[fname] = {}
+            for dname in dlist:
+                rdict[fname][dname] = np.load(fname)
+    if varnames:
+            for varname in varnames:
+                rdict[varname] = {}
+                for dname in dlist:
+                    with open(dname + '/' + outfile) as out:
+                        jout = json.load(out)
+                        rdict[varname][dname] = jout[varname]
+    return rdict
+    # }}}
+
+def data_gen(ds,pts,method):
+# {{{
+    '''
+    Generate energies across a range of displacements, pts
+    This is mostly just to generate extra data points 
+    when doing Margraf-Reuter-type KRR across a PES. 
+    '''
+    with open(ds.inpf,'r') as f:
+        inp = json.load(f)
+    gvar = inp['setup']['gvar'][0]
+    corr_E = []
+
+    psi4.core.clean()
+    psi4.core.clean_options()
+    psi4.core.clean_variables()
+    psi4.set_options({"basis":ds.bas})
+    for i in pts:
+        new_geom = ds.geom.replace(gvar,str(i))
+        mol = psi4.geometry(new_geom)
+        _, wfn = psi4.energy(method,return_wfn=True)
+        corr_E.append(wfn.variable('{} CORRELATION ENERGY'.format(method)))
+    return corr_E
+# }}}
+
+def reg_l2(y,y_p,l,a):
+# {{{
+    '''
+    Calculate L2 (squared) loss with regularization to protect against
+    large norm squared regression coefficients
+    given true and predicted values, regularization, and regression coefficients
+    '''
+    ls = sum([(y_p[i] - y[i])**2 for i in range(len(y))]) + l*np.linalg.norm(a)**2
+    return ls
+# }}}
+
+def grid_search(tr_x,val_x,tr_y,val_y):
+# TODO: it would be great to have a GENERAL grid_search function which takes
+# a prediction function, a training function, and a loss function w/ an
+# arbitrary number of hyperparameters. Will keep this here as a placeholder
+# {{{
+    mse = 1E9
+    s_list = np.logspace(-5,8,num=16)
+    l_list = np.logspace(-8,-1,num=16)
+    for s in s_list:
+        for l in l_list:
+            a = train(tr_x,tr_y,s,l)
+            y_p = pred(val_x,tr_x,a,s,l)
+            new_mse = abs(loss(val_y,y_p,l,a))
+            if new_mse <= mse:
+                s_f = s
+                l_f = l
+                mse = new_mse
+    return s_f, l_f, mse
+# }}}
+
+def legacy_pes_gen(ds, **kwargs):
 # {{{
     '''
     Generate a PES and the representations across it.
@@ -76,57 +226,3 @@ def pes_gen(ds, **kwargs):
     return rep_list,g_E_CORR_list,E_SCF_list
         # }}}
 
-def data_gen(ds,pts,method):
-# {{{
-    '''
-    Generate energies across a range of displacements, pts
-    This is mostly just to generate extra data points 
-    when doing Margraf-Reuter-type KRR across a PES. 
-    '''
-    with open(ds.inpf,'r') as f:
-        inp = json.load(f)
-    gvar = inp['setup']['gvar'][0]
-    corr_E = []
-
-    psi4.core.clean()
-    psi4.core.clean_options()
-    psi4.core.clean_variables()
-    psi4.set_options({"basis":ds.bas})
-    for i in pts:
-        new_geom = ds.geom.replace(gvar,str(i))
-        mol = psi4.geometry(new_geom)
-        _, wfn = psi4.energy(method,return_wfn=True)
-        corr_E.append(wfn.variable('{} CORRELATION ENERGY'.format(method)))
-    return corr_E
-# }}}
-
-def reg_l2(y,y_p,l,a):
-# {{{
-    '''
-    Calculate L2 (squared) loss with regularization to protect against
-    large norm squared regression coefficients
-    given true and predicted values, regularization, and regression coefficients
-    '''
-    ls = sum([(y_p[i] - y[i])**2 for i in range(len(y))]) + l*np.linalg.norm(a)**2
-    return ls
-# }}}
-
-def grid_search(tr_x,val_x,tr_y,val_y):
-# TODO: it would be great to have a GENERAL grid_search function which takes
-# a prediction function, a training function, and a loss function w/ an
-# arbitrary number of hyperparameters. Will keep this here as a placeholder
-# {{{
-    mse = 1E9
-    s_list = np.logspace(-5,8,num=16)
-    l_list = np.logspace(-8,-1,num=16)
-    for s in s_list:
-        for l in l_list:
-            a = train(tr_x,tr_y,s,l)
-            y_p = pred(val_x,tr_x,a,s,l)
-            new_mse = abs(loss(val_y,y_p,l,a))
-            if new_mse <= mse:
-                s_f = s
-                l_f = l
-                mse = new_mse
-    return s_f, l_f, mse
-# }}}
